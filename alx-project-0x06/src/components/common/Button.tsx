6e025e294ecfeab2f7c2cf23068f7330dd18d101
Limitlex Technologies <kobbygazi@gmail.com>
2026-01-13 15:41:12 +0000

    Created package.json and other files

---
+++ alx-project-0x06/src/components/common/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface ButtonProps {
+  buttonLabel: string
+  buttonBackgroundColor?: string
+}
+
+const Button: React.FC<ButtonProps> = ({ buttonLabel, buttonBackgroundColor = 'gray' }) => {
+  const bgClass = `bg-${buttonBackgroundColor}-500 hover:bg-${buttonBackgroundColor}-600 text-white font-semibold py-2 px-4 rounded`;
+
+  // Fallback inline style if Tailwind class doesn't exist
+  return (
+    <button className={bgClass} style={{ minWidth: 80 }}>
+      {buttonLabel}
+    </button>
+  )
+}
+
+export default Button
